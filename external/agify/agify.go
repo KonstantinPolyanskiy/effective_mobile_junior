d8f7ec66b05c1ad0f3cf69cf9f790d8b3d480d48
PolyanskiyKA <polyanskiy.main@list.ru>
2024-01-16 13:52:32 +0300

    Пакет для работы с api.agify.io

---
+++ external/agify/agify.go
@@ -0,0 +1,67 @@
+package agify
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+const BaseResourceURL = "https://api.agify.io/?name="
+
+type Option func(engine Engine)
+
+// WithCustomClient позволяет установить настроенный http.Client
+func WithCustomClient(client *http.Client) Option {
+	return func(engine Engine) {
+		engine.client = client
+	}
+}
+
+type Engine struct {
+	client *http.Client
+}
+
+// NewEngine возвращает объект для работы с ресурсом api.agify.io
+func NewEngine(options ...Option) *Engine {
+	e := &Engine{
+		client: &http.Client{},
+	}
+
+	for _, opt := range options {
+		opt(*e)
+	}
+
+	return e
+}
+
+type Result struct {
+	Count int    `json:"count"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+}
+
+// AgeInfoByName возвращает Result (информацию о возрасте) по переданному имени
+func (e Engine) AgeInfoByName(name string) (Result, error) {
+	var res Result
+
+	// Создаем url вида: https://api.agify.io/?name=Konstantin
+	url := BaseResourceURL + name
+
+	resp, err := e.client.Get(url)
+	if err != nil {
+		return res, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return res, err
+	}
+
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
