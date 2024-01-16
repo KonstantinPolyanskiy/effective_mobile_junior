2ec29d6f016508fe47fd9336cf6e2b4014c1b5e8
PolyanskiyKA <polyanskiy.main@list.ru>
2024-01-16 14:05:54 +0300

    Пакет для работы с api.genderize.io

---
+++ external/genderize/genderize.go
@@ -0,0 +1,68 @@
+package genderize
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+const BaseResourceURL = "https://api.genderize.io/?name="
+
+type Option func(engine Engine)
+
+// WithCustomClient позволяет установить настроенный http.Client
+func WithCustomClient(client *http.Client) Option {
+	return func(engine Engine) {
+		engine.client = client
+	}
+}
+
+type Engine struct {
+	client *http.Client
+}
+
+// NewEngine возвращает объект для работы с ресурсом api.genderize.io
+func NewEngine(options ...Option) *Engine {
+	e := &Engine{
+		client: &http.Client{},
+	}
+
+	for _, opt := range options {
+		opt(*e)
+	}
+
+	return e
+}
+
+type Result struct {
+	Count       int     `json:"count"`
+	Name        string  `json:"name"`
+	Gender      string  `json:"gender"`
+	Probability float64 `json:"probability"`
+}
+
+// GenderInfoByName возвращает Result (информацию о возрасте) по переданному имени
+func (e Engine) GenderInfoByName(name string) (Result, error) {
+	var res Result
+
+	// Создаем url вида: https://api.agify.io/?name=Konstantin
+	url := BaseResourceURL + name
+
+	resp, err := e.client.Get(url)
+	if err != nil {
+		return res, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return res, err
+	}
+
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
