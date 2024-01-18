2ce40b7f59e7be3c243c263e8692762e4b042ce2
PolyanskiyKA <polyanskiy.main@list.ru>
2024-01-18 14:48:37 +0300

    Получение пользователя на слое бизнес логики

---
+++ internal/service/get_person.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"effective_mobile_junior/internal/model"
+	"errors"
+)
+
+func (s Service) GetPerson(params model.GetPersonReq) ([]model.PersonEntity, error) {
+	persons, err := s.Repository.SelectPerson(params)
+	if err != nil {
+		return nil, errors.New("internal server error")
+	}
+
+	return persons, nil
+}
