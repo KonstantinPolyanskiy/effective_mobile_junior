6994f4c595133edae3dc1d957a8abc9651cc65f0
PolyanskiyKA <polyanskiy.main@list.ru>
2024-01-19 14:50:32 +0300

    Коды стран в нижнем регистре

--- internal/service/get_person.go
+++ internal/service/get_person.go
@@ -3,9 +3,13 @@ package service
 import (
 	"effective_mobile_junior/internal/model"
 	"errors"
+	"strings"
 )
 
 func (s Service) GetPerson(params model.GetPersonReq) ([]model.PersonEntity, error) {
+	// Коды стран храним в нижнем регистре
+	params.CountryFilter = strings.ToLower(params.CountryFilter)
+
 	persons, err := s.Repository.GetPersonByParams(params)
 	if err != nil {
 		return nil, errors.New("internal server error")
